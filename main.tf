347f64f8d587f1fae0c3cc69326993017bcc71d9
gauravgupta745 <gaurav.guptagn4@gmail.com>
2026-06-19 02:31:07 +0530

    changes in gurugram

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "rgpaglu"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg" {
+  name     = "rgpaglu1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "rgpaglu2"
+  location = "West Europe"
+}
